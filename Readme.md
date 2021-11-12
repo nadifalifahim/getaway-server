f940964ea4bcb6a0dfa0e29f075a71601276a7fe
Nadif Ali Fahim <nadifalifahim@gmail.com>
2021-11-13 02:09:26 +0600

    adds update

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Getaway Travel Agency Website
+
+## Find The Best Deals For Your Next Getaway. From island getaways, snow holidays to cruise experiences we’ve got a holiday package for it all. Here at Gateaway we pride ourselves on delivering to you the very best in customer experience.
+
+Live Site: https://getaway-travel-agency.web.app/
+
+- Has Private Routing
+- Uses react router
+- Integrated with backend server and database
+- Uses firebase authentication and hosting
+- Made using react
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 const cors = require("cors");
 const app = express();
+const ObjectId = require("mongodb").ObjectId;
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -9,26 +10,86 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Connection with database
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.znerx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
-console.log(uri);
-
+// Database requests
 async function run() {
   try {
     await client.connect();
     const database = client.db("getaway-tour-agency-database");
     const services = database.collection("services");
+    const orders = database.collection("orders");
+
+    app.get("/services", async (req, res) => {
+      const cursor = services.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    app.get("/my-orders", async (req, res) => {
+      const cursor = orders.find({});
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    app.get("/place-order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await services.findOne(query);
+      res.json(result);
+    });
+
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await services.findOne(query);
+      res.json(result);
+    });
+
+    app.post("/add-new-service", async (req, res) => {
+      const newService = req.body;
+      const result = await services.insertOne(newService);
+      res.json(result);
+    });
+
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orders.deleteOne(query);
+      res.json(result);
+    });
+
+    app.patch("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          orderStatus: "Approved",
+        },
+      };
+      const result = await orders.updateOne(query, updateDoc);
+      res.json(result);
+    });
+
+    app.post("/new-order", async (req, res) => {
+      const newOrder = req.body;
+      console.log(newOrder);
+      const result = await orders.insertOne(newOrder);
+      res.json(result);
+    });
   } finally {
-    await client.close();
+    // await client.close();
   }
 }
 
 run().catch(console.dir);
 
+// Checking backend functionality
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
